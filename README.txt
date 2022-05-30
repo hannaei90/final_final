4c144330c151890794af8df4f95a330a1699e4ae
sannsch <104627657+sannsch@users.noreply.github.com>
2022-05-30 21:02:34 +0200

    finished

---
+++ README.txt
@@ -0,0 +1,6 @@
+Assuming that python and an IDE already is installed on the computer.
+Download the zip file and open controller.py in a virtual environment.
+Download flask and sqlite3 in the terminal (pip install flask/sqlite3/pysqlite3).
+Run controller.py and open the local server.
+On the website, write a location in "Plats", the available locations are in our database:
+    places2.db [ica, coop, hemköp, synoptik, synsam, postnord, posten, polisen, frisören, migrationsverket, skatteverket].
--- database.py
+++ database.py
@@ -1,34 +1,14 @@
 import sqlite3
 
-conn = sqlite3.connect('user2.db')
+conn = sqlite3.connect('places2.db')
 c = conn.cursor()
 # c.execute('CREATE TABLE places(place text)')
 #c.execute('CREATE TABLE user(email text, password text)')
 
-def create_user(email, password):
-    con = sqlite3.connect('user2.db')
-    c = con.cursor()
-    c.execute("SELECT* FROM user WHERE email=:email AND password =:password",{'email':email, 'password':password})
-    data = c.fetchone()
-    if data == None:
-        c.execute("INSERT INTO user (email,password) VALUES (?,?)", (email, password))
-        con.commit()
-        con.close()
-        return 1
-    else:
-        con.close()
-        return 2
+#c.execute("INSERT INTO places (place) VALUES (?)", (''))
 
-def check_user(email, password):
-    con = sqlite3.connect('user2.db')
-    c = con.cursor()
-    c.execute("SELECT* FROM user WHERE email=:email AND password =:password", {'email': email, 'password': password})
-    data = c.fetchone()
-    con.close()
-    return data
+c.execute("INSERT INTO places VALUES ('') ")
 
-test = check_user('sandra', 'äpple')
-print(test)
 
 conn.commit()
 
